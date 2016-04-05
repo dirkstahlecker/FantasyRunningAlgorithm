74c192ebd57a4c1302f124ee54316aaa16b0e576
Dirk <stahdirk@mit.edu>
2016-04-04 21:53:30 -0400

    added basic averaging analytics; known but when running with pages over 35, duration average becomes -5000 for some reason yet unknown

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import urllib2
 import json
 
+################ constants ###############################################
 TIER1_DURATION = 60
 TIER2_DURATION = 40
 TIER3_DURATION = 20
@@ -11,10 +12,11 @@ TIER1_SPEED = 8
 TIER2_SPEED = 4
 TIER3_SPEED = 2
 
-NUM_PAGES = 10
+NUM_PAGES = 35
 
+################ methods ###############################################
 def makeRequest(page):
-    req = urllib2.Request('https://pumatrac-geo-api.herokuapp.com/activities?bounds=box:0,0:90,180') #get all data
+    req = urllib2.Request('https://pumatrac-geo-api.herokuapp.com/activities?bounds=box:0,0:90,180&page=' + str(page)) #get all data
     req.add_header('Authorization', 'Bearer 1cfb51cd69904221818dafc4069f9d61')
     resp = urllib2.urlopen(req)
     content = resp.read()
@@ -37,14 +39,28 @@ def addToLists(decoded_json):
         mean_speed = activity['mean_speed']
         mean_speed_list.append(mean_speed)
 
+def doAnalytics():
+    #average of each
+    print 'Average duration: ',
+    duration_avg = float(sum(duration_list)) / len(duration_list)
+    print duration_avg
+    print 'Average mean speed ',
+    mean_speed_avg = float(sum(mean_speed_list)) / len(mean_speed_list)
+    print mean_speed_avg
+
+################ execution ###############################################
+
 #populate the lists from the API data
 for x in range(0,NUM_PAGES):
     addToLists(makeRequest(x))
 
+doAnalytics()
 
-
-print duration_list
-print mean_speed_list
+#print duration_list
+#print mean_speed_list
 
 
 #utilize distance, duration, mean speed
+
+
+
